23a96c76315f0a97a5210efc271eba81bd5629cd
wjchenqing <wjchenqing@sjtu.edu.cn>
2021-04-18 18:38:06 +0800

    Did nothing.

--- src/Codegen/Operand/RegisterPhysical.java
+++ src/Codegen/Operand/RegisterPhysical.java
@@ -1,9 +1,6 @@
 package Codegen.Operand;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class RegisterPhysical extends Register {
     static public String[] Names = {
@@ -16,17 +13,17 @@ public class RegisterPhysical extends Register {
     };
     static public String[] colors = {
             "ra",                                                           //1         //0
-            "t0", "t1", "t2",                                               //5 ~ 7     //1, 2, 3
             "s0", "s1",                                                     //8, 9      //4, 5
-            "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",                 //10 ~ 17   //6  ~ 13
             "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",   //18 ~ 27   //14 ~ 23
+            "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",                 //10 ~ 17   //6  ~ 13
+            "t0", "t1", "t2",                                               //5 ~ 7     //1, 2, 3
             "t3", "t4", "t5", "t6"                                          //28 ~31    //24 ~ 27
     };
     static public int[] calleeSaveNum = {
             8, 9, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27
     };
-    static public Map<String, RegisterPhysical> physicalRegisterSet = new HashMap<>();
-    static public Set<RegisterPhysical> colorSet = new HashSet<>();
+    static public Map<String, RegisterPhysical> physicalRegisterSet = new LinkedHashMap<>();
+    static public Set<RegisterPhysical> colorSet = new LinkedHashSet<>();
     static public Map<String, RegisterVirtual> virtualMap = new HashMap<>();
 
     static {
